b1982bccc493f0be30638ba3796027c786c7051d
IComplainInComments <IComplainInComments@users.noreply.github.com>
2021-05-12 14:29:56 -0400

    Initial Code
    Initial Code used to generate a semi-working profile for generating a UEFI bootable ISO.

---
+++ configs/aarch64/profiledef.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="Arch Linux Arm"
+iso_label="Arch Linux Arm"
+iso_publisher="IComplainInComments"
+iso_application="Generic ARM64 UEFI install/Rescue image"
+bootmodes=('uefi-aarch64.systemd-boot.esp' 'uefi-aarch64.systemd-boot.eltorito')
+arch="aarch64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
