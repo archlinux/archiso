ee2a721cea8a1f230f570ae93bac8d94142cb6b1
nl6720 <nl6720@gmail.com>
2022-04-04 15:18:55 +0300

    Revert "Merge remote-tracking branch 'origin/merge-requests/239'"
    People get scared by it. See https://gitlab.archlinux.org/archlinux/archiso/-/merge_requests/239#note_61954.
    
    This reverts commit b5011af3f4ac63214fdecef442550cce5ae4a971, reversing
    changes made to 07d8035624cff64e28f1148ddec9e970e26173da.

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -20,5 +20,4 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/pacman-beep.sh"]="0:0:755"
 )
