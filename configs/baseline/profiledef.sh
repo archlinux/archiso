11b01852932be66e6bd5f022f52eb9258412affd
xundaoxd <xundaoxd@outlook.com>
2022-10-12 00:03:48 +0800

    use squashfs as iso type

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -13,8 +13,8 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )
