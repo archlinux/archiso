db51a58302b9689ec26270bf157694fc67e15d54
xundaoxd <xundaoxd@outlook.com>
2022-10-12 12:19:11 +0800

    setup xundaoxd account

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -16,5 +16,8 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
+  ["/etc/passwd"]="0:0:400"
   ["/etc/shadow"]="0:0:400"
+  ["/etc/group"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
 )
