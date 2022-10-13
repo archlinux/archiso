19858df441a042d7ec1865748afa05e3dadeec41
xundaoxd <xundaoxd@outlook.com>
2022-10-13 11:34:05 +0800

    install origin dwm

--- configs/airootfs/root/customize_airootfs.sh
+++ configs/airootfs/root/customize_airootfs.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-systemctl enable iwd.service
-systemctl enable systemd-networkd
-systemctl enable systemd-resolved
-systemctl enable sshd
-
 hostname="archiso"
 echo $hostname > /etc/hostname
 
@@ -14,22 +9,22 @@ echo -e 'en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 locale-gen
 
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+pacman-key --init
+pacman-key --populate
+
+systemctl enable iwd
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable sshd
+
 # setup dwm
-git clone https://gitee.com/xundaoxd/dogeystamp-dwm.git
-cd dogeystamp-dwm
-make
-sudo make install
-cd ..
-git clone https://gitee.com/xundaoxd/dogeystamp-st.git
-cd dogeystamp-st
-make
-sudo make install
-cd ..
 git clone https://gitee.com/xundaoxd/dogeystamp-dmenu.git
-cd dogeystamp-dmenu
-make
-sudo make install
-cd ..
+(cd dogeystamp-dmenu && make && make install)
+git clone https://gitee.com/xundaoxd/dogeystamp-st.git
+(cd dogeystamp-st && make && make install)
+git clone https://gitee.com/xundaoxd/archlinux-dwm.git
+(cd archlinux-dwm && make && make install)
 
 chsh -s /bin/zsh
 cp /etc/X11/xinit/xinitrc /root/.xinitrc
