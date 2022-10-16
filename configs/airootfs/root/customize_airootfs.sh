c3e18ee9deb80de3fb352b2b07b87a9f6d6bb221
xundaoxd <xundaoxd@outlook.com>
2022-10-16 11:38:28 +0800

    mount home vol, install virt-manager

--- configs/airootfs/root/customize_airootfs.sh
+++ configs/airootfs/root/customize_airootfs.sh
@@ -13,10 +13,14 @@ sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
 pacman-key --init
 pacman-key --populate
 
+usermod -aG wheel,input,video,audio,kvm xundaoxd
+sed -E -i '/^#\s*%wheel.*NOPASSWD/{s/^#\s*//}' /etc/sudoers
+
 systemctl enable NetworkManager
 systemctl enable sddm
 systemctl enable sshd
 
-usermod -aG wheel,input,video,audio,kvm xundaoxd
-sed -E -i '/^#\s*%wheel.*NOPASSWD/{s/^#\s*//}' /etc/sudoers
+systemctl enable libvirtd
+sed -i 's/^#unix_sock_group/#unix_sock_group/;s/^#unix_sock_rw_perms/unix_sock_rw_perms/' /etc/libvirt/libvirtd.conf
+usermod -aG libvirt xundaoxd
 
