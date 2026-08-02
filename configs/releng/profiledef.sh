ce5fb8b041e9dd0e6d2fad756c73e4a6f1e45039
Clover <alex@ironrobin.net>
2026-08-01 20:38:48 -0500

    releng: add AArch64 boot compatibility parameters
    Some AArch64 systems depend on clocks and power domains left enabled by
    firmware because the corresponding drivers do not claim them early enough.
    Keep those resources enabled during boot and disable pointer authentication
    to work around firmware issues on affected Snapdragon platforms.
    
    These parameters were found in /boot/grub/grub.cfg on the Ubuntu Generic AArch64
    ISO:
    
        clk_ignore_unused pd_ignore_unused arm64.nopauth
    
    Apply them to the releng systemd-boot entries only when building for
    AArch64.
    
    Ubuntu Generic AArch64 ISO: https://cdimage.ubuntu.com/ubuntu/releases/resolute/release/ubuntu-26.04-desktop-arm64.iso
    
    Related: https://gitlab.archlinux.org/archlinux/ports/aarch64/project-management/-/work_items/7

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -13,6 +13,7 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+kernel_params_aarch64="clk_ignore_unused pd_ignore_unused arm64.nopauth"
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
